67b1a01b94151e8deb5d9685af12bed797991da7
chao.wu <2662259266@qq.com>
2021-05-03 13:20:23 +0800

    init

--- assets/about.e16bc721.js
+++
@@ -1,1 +0,0 @@
-import{o,c as r}from"./vendor.7048643e.js";const e={};e.render=function(e,n){return o(),r("div",null," 哈哈按计划喀喀喀 head root ")};export default e;
--- assets/index.88eb1da7.js
+++
@@ -1,1 +0,0 @@
-import{o,c as r}from"./vendor.7048643e.js";const t={};t.render=function(t,e){return o(),r("div",null," about about root ")};export default t;
